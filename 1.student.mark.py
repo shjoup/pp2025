2c2adab35d05cca022a1eb04a98dead635d21846
Shoup <shoupkuro@gmail.com>
2025-11-29 10:02:03 +0700

    First commit

---
+++ 1.student.mark.py
@@ -0,0 +1,18 @@
+nostudent = input("Enter the number of student: ")
+studentid = input("Enter the student id: ")
+studentname = input("Enter student name: ")
+studentdob = input("Enter student dob: ")
+nocourse = input("Enter number of course: ")
+courseid = input("Enter course id: ")
+coursename = input("Enter course name: ")
+mark = input("Enter mark: ")
+studentmarkdict = {"Name": studentname, "Student id": studentid, "Student name": studentname, "Student DoB": studentdob, "Course id": courseid, "Coursename": coursename, "Mark": mark}
+
+key = input("Enter key to modify: ")
+
+if key in studentmarkdict:
+    value = input(f"Enter the new value for '{key}': ")
+    studentmarkdict[key] = value
+    print(studentmarkdict)
+else:
+    print("Key not found")
