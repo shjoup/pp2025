c86cf817b1c46a81b7853d6c61dcb488ef1d90fe
Shoup <shoupkuro@gmail.com>
2025-11-29 10:23:15 +0700

    Change course id modifier

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -6,13 +6,14 @@ nocourse = input("Enter number of course: ")
 courseid = input("Enter course id: ")
 coursename = input("Enter course name: ")
 mark = input("Enter mark: ")
-studentmarkdict = {"Name": studentname, "Student id": studentid, "Student name": studentname, "Student DoB": studentdob, "Course id": courseid, "Coursename": coursename, "Mark": mark}
+studentmarkdict = {"Name": studentname, "Student id": studentid, "Student name": studentname, "Student DoB": studentdob, "Course id": courseid, coursename: coursename, "Mark": mark}
 
 key = input("Enter course to modify: ")
 
 if key in studentmarkdict:
     value = input(f"Enter the new mark for '{key}': ")
     studentmarkdict[mark] = value
+
     print(studentmarkdict)
 
 else:
